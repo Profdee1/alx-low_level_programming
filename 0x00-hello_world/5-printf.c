2f80be05b540ec170ec57b346788e626ed9a5088
Malcolm <adjei.malcolm2@gmail.com>
2023-06-17 16:09:21 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+  *main - Hello, printf
+  *Description: 'Proper grammar, piece of art'
+  *Return: always 0 (success)
+  */
+int main(void)
+{
+	printf('with proper grammar, but the outcome is a piece of art,\n');
+	return (0);
+}
