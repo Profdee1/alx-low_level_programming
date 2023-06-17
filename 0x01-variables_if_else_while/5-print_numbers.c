1d7c1d37aab7656402f089ec74a3547804c6a252
Malcolm <adjei.malcolm2@gmail.com>
2023-06-17 12:37:53 -0700

    Numbers in base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print numbers
+ * Description: 'All digit numbers in base 10 start from 0
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
