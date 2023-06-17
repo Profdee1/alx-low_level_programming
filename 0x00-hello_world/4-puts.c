cf61d3679807f59bfbe1fe20ac118034ebf70898
Malcolm <adjei.malcolm2@gmail.com>
2023-06-17 15:49:10 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+  *main - Hello, puts
+  *Description: 'Print exactly Programming'
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int puts;
+
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
