4d67f79944eaa6b3ab1f5ab0c3cd8e6cbf8e5e0f
Malcolm <adjei.malcolm2@gmail.com>
2023-06-17 18:55:19 -0700

    Inventing is a combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+  *main - Inventing
+  *Description: 'The more brains you use, the less material you need'
+  *Return: always 0
+  */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+	for (m = 49; m <= 57; n++)
+	{
+		if (m > n)
+		{
+			putchar(n);
+			putchar(m);
+		if (n == 56 || m ==57)
+		{
+			continue;
+		}
+	putchar(',');
+	putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
