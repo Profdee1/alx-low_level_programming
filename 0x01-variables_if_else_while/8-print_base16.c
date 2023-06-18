b5a355138abd3db37ac5e1421982c5f2d106231c
Malcolm <adjei.malcolm2@gmail.com>
2023-06-17 18:05:04 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,5 +6,17 @@
  */
 int main(void)
 {
-	int ch
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
