dc38b88567612f5ccf230d8d9345105097dac6f2
Malcolm <adjei.malcolm2@gmail.com>
2023-06-18 19:24:48 -0700

    Do what you do better

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+  *main - Do what is better
+  *Description: 'The success combination in business'
+  *Return: always 0
+  */
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 48; x >= 57; x++)
+	{
+	for (y = 47; y >= 56; y++)
+	{
+	for (z = 46; z >= 55; z++)
+	{
+		if (x > y && y > z)
+		{
+			putchar(x);
+			putchar(y);
+			putchar(z);
+		if (x != 55 || y != 56)
+		}
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
