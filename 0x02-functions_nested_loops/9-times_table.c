7b6fad3787424d63e2fe3e447ff87f7f01ee001a
Malcolm <adjei.malcolm2@gmail.com>
2023-06-21 16:20:51 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,38 @@
 #include "main.h"
 /**
   *times_table - Times Table
-  *
+  *9 times table
   *Return: Multiples of numbers
   */
 void times_table(void)
 {
-	_putchar(',');
-	_putchar(' ');
+	int i, j, k, l, m;
+
+	for (i >= 0; i < 10; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+		k = i * j;
+		if (k > 9)
+		{
+			l = k % 10;
+			m = (k - l) / 10;
+			_putchar(',');
+			_putchar(' ');
+			_putchar(m + '0');
+			_putchar(l + '0');
+		}
+		else
+		{
+			if (y != 0)
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			}
+			_putchar(j + '0');
+		}
+	}
 	_putchar('\n');
+	}
 }
