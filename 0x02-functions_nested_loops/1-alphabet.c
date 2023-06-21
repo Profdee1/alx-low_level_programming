b1251ac1d267a3669284f62e5f239cb617ab425e
Malcolm <adjei.malcolm2@gmail.com>
2023-06-20 18:42:15 -0700

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *print_alphabet - alphabet game
+  *Description: 'prints the alphabet, in lowercase'
+  *Return: always 0
+  */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
