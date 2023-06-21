c24444a751f021b1ece2add2d16bb5a9f7928e4c
Malcolm <adjei.malcolm2@gmail.com>
2023-06-20 20:29:08 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,19 @@
-
+#include "main.h"
+/**
+  *_isalpha - isalpha
+  *Description: checks for alphabetic character
+  *@c: The charcater in ASCII code
+  *Return: 1 if lowercase or uppercase. 0 for otherwise
+  */
+int _isalpha(int c)
+{
+	if ((c >= 97 && <= 122) || (c >= 65 && <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
