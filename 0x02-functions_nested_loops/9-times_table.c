6be9af827937afe4c731f855454c170b9e177780
Malcolm <adjei.malcolm2@gmail.com>
2023-06-20 22:29:55 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  *times_table - Times Table
+  *
+  *Return: Multiples of numbers
+  */
+void times_table(void)
+{
+	for (
+	_putchar(',');
+	_putchar(' ');
+	_putchar('\n');
+}
