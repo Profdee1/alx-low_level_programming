dbe2bf6cb441d69524f51cc42912b09132b54163
Malcolm <adjei.malcolm2@gmail.com>
2023-06-20 21:18:17 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_abs - Absolute value in this world
+  *Description: 'You can only estimate what a thing is worth'
+  *@i: integer number inputted
+  *Return: absolute number
+  */
+int _abs(int i)
+{
+	if (i <= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
