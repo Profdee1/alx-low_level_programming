156792bd713899cde80b79f5d104f2b82e492f1f
Malcolm <adjei.malcolm2@gmail.com>
2023-06-21 17:29:19 -0700

    98 Battrey Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
   *print_to_98 - Print to 98
   *@n: numbers
@@ -12,13 +13,13 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				_putchar('%d', n);
-				_putchar('\n');
+				printf('%d', n);
+				printf('\n');
 				break;
 			}
 			else
 			{
-				_putchar('%d, ', n);
+				printf('%d, ', n);
 			}
 		}
 	}
@@ -28,13 +29,13 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				_putchar('%d', n);
-				_putchar('\n');
+				printf('%d', n);
+				printf('\n');
 				break;
 			}
 			else
 			{
-				_putchar('%d, ', n);
+				printf('%d, ', n);
 			}
 		}
 	}
